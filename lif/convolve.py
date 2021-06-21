190e70a322a00efb92102462fd0550df98399e75
Errol Lloyd <errolkjlloyd@gmail.com>
2021-06-21 23:36:40 +1000

    Add convolution code/module and test notebook

---
+++ lif/convolve.py
@@ -0,0 +1,92 @@
+"""Using receptive fields and stimuli to create firing rates through convolution
+"""
+from typing import Tuple
+
+from brian2.monitors.ratemonitor import PopulationRateMonitor
+from brian2.monitors.spikemonitor import SpikeMonitor
+import numpy as np
+from scipy.signal import convolve
+
+import brian2 as bn
+
+from .stimulus import stimulus as stim
+from .receptive_field.filters import filter_functions as ff
+
+from .utils import data_objects as do
+
+
+def mk_single_sf_tf_response(
+        sf: do.DOGSpatialFilter,
+        tf: do.TQTempFilter,
+        st_params: do.SpaceTimeParams,
+        stim_params: do.GratingStimulusParams,
+        rectified: bool = True
+        ) -> np.ndarray:
+    """Produces rectified 1D response of "cell" defined by sf+tf to stim
+
+    stim grating presumed to be 3D (temporal)
+
+    Stimulus and filters are created to the same extent as defined in st_params
+    """
+
+    xc, yc = ff.mk_spat_coords(st_params.spat_res, st_params.spat_ext)
+    tc = ff.mk_temp_coords(st_params.temp_res, st_params.temp_ext)
+
+    spat_filt = ff.mk_dog_sf(xc, yc, sf.parameters)
+    temp_filt = ff.mk_tq_tf(tc, tf.parameters)
+
+    grating = stim.mk_sinstim(st_params, stim_params)
+
+    spatial_product = (spat_filt[..., np.newaxis] * grating).sum(axis=(0, 1))
+
+    resp: np.ndarray = convolve(spatial_product, temp_filt)[:tc.value.size]
+
+    adj_params = ff.mk_conv_resp_adjustment_params(
+        st_params, stim_params, sf, tf)
+
+    true_resp = ff.adjust_conv_resp(resp, adj_params)
+
+    if rectified:
+        true_resp[true_resp < 0] = 0
+
+    return true_resp
+
+
+def mk_sf_tf_poisson(
+        st_params: do.SpaceTimeParams,
+        resp: np.ndarray, n_trials: int = 20
+        ) -> Tuple[bn.SpikeMonitor, bn.PopulationRateMonitor]:
+
+    bn.start_scope()
+
+    # number of brian clock steps per temporal res of sf_tf response
+    # n_clock_steps = st_params.temp_res.s * bn.second / bn.defaultclock.dt
+
+    # Turn response rate to brian array of rates, with appropriate time step
+    I_recorded = bn.TimedArray(  # noqa: F841
+        resp * bn.Hz, dt=st_params.temp_res.s * bn.second)  # type: ignore
+
+    cell = bn.PoissonGroup(n_trials, rates='I_recorded(t)')
+    spikes = bn.SpikeMonitor(cell)
+    pop_spikes = bn.PopulationRateMonitor(cell)
+
+    bn.run(st_params.temp_ext.s * bn.second)  # type: ignore
+
+    return spikes, pop_spikes
+
+
+def aggregate_poisson_trials(spikes_record: bn.SpikeMonitor) -> np.ndarray:
+
+    cells, spike_times = [], []
+
+    for cell, spikes in spikes_record.spike_trains().items():
+        # conversion to array necessary to take away brian cruft
+        cells.append(np.ones_like(np.array(spikes)) * cell)
+        spike_times.append(np.array(spikes))
+
+    cells = np.concatenate(cells)
+    spikes = np.concatenate(spike_times)
+
+    all_spikes = np.stack([cells, spikes], axis=0)
+
+    return all_spikes
---
+++ lif/poisson_test_nb.py
@@ -0,0 +1,157 @@
+# ===========
+from lif import *
+# -----------
+# ===========
+import plotly.express as px
+from scipy.ndimage import gaussian_filter1d
+# -----------
+
+# > Poisson Response and Plots
+# ===========
+tf = TQTempFilter.load(TQTempFilter.get_saved_filters()[0])
+sf = DOGSpatialFilter.load(DOGSpatialFilter.get_saved_filters()[0])
+# -----------
+# ===========
+# stim_amp=0.5
+spat_res=ArcLength(1, 'mnt')
+spat_ext=ArcLength(120, 'mnt')
+temp_res=Time(1, 'ms')
+temp_ext=Time(1000, 'ms')
+
+orientation = ArcLength(90, 'deg')
+temp_freq = TempFrequency(8)
+spat_freq_x = SpatFrequency(2)
+spat_freq_y = SpatFrequency(0)
+# -----------
+# ===========
+st_params = do.SpaceTimeParams(spat_ext, spat_res, temp_ext, temp_res)
+stim_params = do.GratingStimulusParams(
+    spat_freq_x, temp_freq,
+    orientation=orientation,
+    amplitude=1, DC=1
+)
+# -----------
+# ===========
+resp = conv.mk_single_sf_tf_response(sf, tf, st_params, stim_params)
+# -----------
+# ===========
+s, pop_s = conv.mk_sf_tf_poisson(st_params, resp)
+# -----------
+# ===========
+all_spikes = conv.aggregate_poisson_trials(s)
+# -----------
+# ===========
+(px
+    .scatter(x=all_spikes[1], y=all_spikes[0],
+             template='none')
+    .update_traces(marker=dict(
+        symbol='line-ns', line_width=1, color='black'))
+    .show()
+)
+# -----------
+# ===========
+n_trials = 20
+bin_width = Time(0.01)
+bins = ff.mk_temp_coords(bin_width, Time(st_params.temp_ext.s + bin_width.s))
+
+cnts, cnt_bins = np.histogram(all_spikes[1], bins=bins.base)
+cnts = cnts / bin_width.s / n_trials
+cnts_smooth = gaussian_filter1d(cnts, sigma=1)
+
+(
+    px
+    .bar(x=cnt_bins[:-1], y=cnts, template='none')
+    .update_traces(marker_color='#bbb')
+    .add_trace(
+        px.line(x=cnt_bins[:-1], y=cnts_smooth)
+        .update_traces(line_color='red')
+        .data[0]
+        )
+    .show()
+    )
+# -----------
+
+# > ori bias
+# ===========
+def cv(r, theta):
+    """
+    Circular Variance (Ringach (2000))
+    """
+    x = np.sum((r * np.exp(1j * 2 * theta))) / np.sum(r)
+    # return (1 - np.abs(x))
+    return (np.abs(x))
+
+def vm(x, a=1, k=0.5, phi=np.pi / 2):
+    """
+    Single von mises function (Swindale (1998))
+    """
+
+    return a * np.exp(k * (np.cos(phi - x)**2 - 1))
+
+# -----------
+# ===========
+angles = ArcLength(np.linspace(0, 180, 8, False), 'deg')
+k_vals = np.linspace(0, 30, 1000)
+
+cv_vals = [
+    cv(vm(angles.rad, k=k_val), angles.rad)
+    for k_val in k_vals
+]
+# -----------
+# ===========
+px.line(x=k_vals, y=cv_vals).show()
+# -----------
+# ===========
+k_val = 0.6
+a, b = vm(0, k=k_val), vm(np.pi/2, k=k_val)
+a,b
+# -----------
+# ===========
+target_amp = (a + b)/2
+ori_ratio = b/a
+target_amp, ori_ratio
+# -----------
+# ===========
+ff.mk_dog_sf_ft(SpatFrequency(10), SpatFrequency(0), sf.parameters)
+# -----------
+# ===========
+sf.parameters.cent.arguments.asdict_()
+# -----------
+# ===========
+amps = vm(angles.rad, k=0.6)
+# -----------
+# ===========
+px.line(x=angles.deg, y=amps).show()
+# -----------
+
+
+
+
+# ===========
+angles = ArcLength(np.arange(0, 180, 10), 'deg')
+
+vm_amp = vm(angles.rad, k=2)
+# -----------
+# ===========
+px.line(x=angles.base, y=vm_amp).show()
+# -----------
+# ===========
+from scipy.stats import vonmises
+# -----------
+# ===========
+angles = np.arange(0, 180, 10)
+angles
+# -----------
+# ===========
+cv(
+    np.array([2, 1, 3, 2]),
+        np.deg2rad(np.array([0, 45, 90, 135]))
+    )
+# -----------
+
+# ===========
+# preferred is this much greater response than non-preferred
+bias_factor = 1.5
+# -----------
+# ===========
+# -----------
